76147bc53bf42de21f6e9898b17f5302080f02e7
Daren <darent3@gmail.com>
2026-01-11 17:06:56 -0500

    Add body field to HomePage model

--- home/models.py
+++ home/models.py
@@ -1,7 +1,27 @@
 from django.db import models
-
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    """
+    The home page model - the root page of the website
+    """
+    
+    # Define the fields that appear in the admin
+    body = RichTextField(
+        blank=True,
+        help_text="The main content for the home page"
+    )
+    
+    # Tell Wagtail which fields to show in the editor
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+    
+    # This makes the HomePage template use a specific template
+    template = 'home/home_page.html'
+    
+    class Meta:
+        verbose_name = "Home Page"
